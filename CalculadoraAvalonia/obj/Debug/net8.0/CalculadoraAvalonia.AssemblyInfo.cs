7fc985c0f2a9b63dafd98daa966b64439201af33
Daniel-Lima <ldaniel9090@gmail.com>
2024-12-25 21:07:08 -0300

    Set Styles

--- CalculadoraAvalonia/obj/Debug/net8.0/CalculadoraAvalonia.AssemblyInfo.cs
+++ CalculadoraAvalonia/obj/Debug/net8.0/CalculadoraAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d833a3a579fc555478dc35f1c3c3d60bb9a1561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebede7ecbf1b64ac0a94dba06e8cace1ca63d71f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
